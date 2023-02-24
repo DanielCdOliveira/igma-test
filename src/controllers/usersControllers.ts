bd2a91f9a58fdfa0379191235c3cfb4234ffef06
Daniel Oliveira <danielcoliv98@gmail.com>
2023-02-24 19:11:24 -0300

    route get user by cpf

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -3,6 +3,10 @@ import { CreateUserData } from "../interfaces/prismaInterfaces.js";
 import CpfServices from "../services/cpfServices.js";
 import UsersServices from "../services/userServices.js";
 
+interface Query {
+  cpf: string;
+}
+
 class UsersController {
   constructor(
     private usersService = new UsersServices(),
@@ -15,8 +19,22 @@ class UsersController {
     res.sendStatus(201);
   }
 
-  getUser(req: Request, res: Response) {
-    res.send("Hello World!");
+  public async getUser(req: Request, res: Response) {
+    const { cpf } = req.query as unknown as Query;
+    if (!cpf) {
+      throw {
+        type: "unprocessable",
+        message: "empty cpf query",
+      };
+    }
+    const user = await this.usersService.getUserByCpf(cpf);
+    if (!user) {
+      throw {
+        type: "not_found",
+        message: "cpf not registered",
+      };
+    }
+    res.status(200).send(user);
   }
 
   getAllUsers(req: Request, res: Response) {
